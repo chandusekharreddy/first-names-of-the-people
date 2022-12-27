6375e1ab1791d3a0531b93a5a9d667098f55d237
chandusekharreddy <kakurichandrasekharreddy@gmail.com>
2022-12-27 04:53:33 +0000

    main

---
+++ names/index.js
@@ -0,0 +1,8 @@
+const list = require("../country/state/city/index");
+const getFirstNames = require("../utilities/utils/index");
+
+const getPeopleInCity = (list) => {
+  return getFirstNames(list);
+};
+
+module.exports = getPeopleInCity;
